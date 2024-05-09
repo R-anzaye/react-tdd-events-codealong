d6039e9cc38e797db6363b26026748c57c6bbed8
Ryan Anzaye <rileydollar8@gmail.com>
2024-05-10 01:46:29 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
 function App() {
+  const [ toppingChecked, setToppingChecked ] = useState(false);
+
+  const toggleTopping = (e) => setToppingChecked(e.target.checked);
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Create Your Pizza</h1>
+      <input 
+        type="checkbox" 
+        id="topping" 
+        checked={toppingChecked}
+        aria-checked={toppingChecked}
+        onChange={toggleTopping}
+      />
+      <label htmlFor="topping">Add your favorite topping</label>
+
+      <h2>Your Pizza:</h2>
+      <ul>
+        <li>Cheese</li>
+        {toppingChecked ? <li>Your selected topping</li> : null}
+      </ul>
+
+    </div>
+  );
 }
 
 export default App;
